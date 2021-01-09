3223f55085ee5858f654a40537a855bae7abe449
Dimple Arora <jatindera@gmail.com>
2021-01-09 14:04:06 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+var mqtt = require('mqtt')
+
+var options = {
+    username: '*****',
+    password: '*****',
+    port: 1883
+}
+
+var client  = mqtt.connect('mqtt://maqiatto.com', options)
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/', async (req, res) => {
+
+    let command = ""
+    // console.log(req.body)
+    if (req.body.queryResult.intent.displayName === "light-intent") {
+        command = req.body.queryResult.parameters["light-command"]
+        console.log(`===========Command is to turn ${command} the light===========`)
+        client.publish('*****.com/ramukaka', command)
+    }
+    let resText = `Going to turn ${command} the light`
+    res.json({
+        "fulfillmentMessages": [
+            {
+                "text": {
+                    "text": [
+                        resText
+                    ]
+                }
+            }
+        ]
+    })
+})
+
+app.listen(process.env.PORT)
